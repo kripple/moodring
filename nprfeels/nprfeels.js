943d21088d7ced5a4427c1029fcfdeddc41e5a3b
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-11-08 02:09:20 -0500

    add support for multiple articles

--- nprfeels/nprfeels.js
+++ nprfeels/nprfeels.js
@@ -29,38 +29,40 @@ if (Meteor.isServer) {
     // var watson = Meteor.npmRequire('watson-developer-cloud');
     // console.log(watson);
 
-    var url = 'http://api.npr.org/query?date.current&numResults=1&apiKey=MDExMDE5NDY5MDEzNjI4NzgwNTc3MjMxMw001&output=JSON&fields=title,text,teaser,image';
+    var url = 'http://api.npr.org/query?date.current&numResults=20&apiKey=MDExMDE5NDY5MDEzNjI4NzgwNTc3MjMxMw001&output=JSON&fields=title,text,teaser,image';
 
     Meteor.methods({
       'request': function() {
         return Meteor.http.call('GET', url, {}, function(error, jsonData) {
-          var story = jsonData['data']['list']['story'][0];
-          var storyId = story['id'];
-          var storyUrl = story['link'][0]['$text'];
-          var storyTitle = story['title']['$text'];
-          var storyTeaser = story['teaser']['$text'];
-          var paragraphs = story['text']['paragraph'];
 
-          var storyText = "";
-          for(var i = 0; i < paragraphs.length; i++) {
-            storyText += paragraphs[i]['$text'];
-          };
+          var stories = jsonData['data']['list']['story'];
+          for(var i = 0; i < stories.length; i++){
+            var story = stories[i];
+            var storyId = story['id'];
+            var storyUrl = story['link'][0]['$text'];
+            var storyTitle = story['title']['$text'];
+            var storyTeaser = story['teaser']['$text'];
+            var paragraphs = story['text']['paragraph'];
 
-          // analyze text
+            var storyText = "";
+            for(var j = 0; j < paragraphs.length; j++) {
+              storyText += paragraphs[j]['$text'];
+            };
 
-          var url = 'https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone';
+            // analyze text
 
-          // console.log(storyText);
+            var url = 'https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone';
+            var authKey = "0dbedc88-6434-4ecc-921e-952eba4261c1:v6zxf14AqPDs"
 
-          Meteor.http.call('POST', url, 
-            { auth: "0dbedc88-6434-4ecc-921e-952eba4261c1:v6zxf14AqPDs", 
-            data: {"text": storyText},
-            headers: {"content-type":"text/plain"}
-          }, function(error, data) {
-            // console.log(error);
-            console.log(data['data']['children'][0]);
-            console.log(data['data']['children'][0]['children']);
-          });
+            Meteor.http.call('POST', url, { auth: authKey, data: {"text": storyText},
+              headers: {"content-type":"text/plain"}
+            }, function(error, data) {
+              // console.log(error);
+              // console.log(data['data']['children'][0]);
+              // console.log(data['data']['children'][0]['children']);
+            });
+
+          }
         
           // example post:
           // {

d5d3b9d9202ceacd93704b3f1d85ad8063fc4f4a
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-11-07 21:49:43 -0500

    working on sending data from server to client

--- nprfeels/nprfeels.js
+++ nprfeels/nprfeels.js
@@ -1,4 +1,21 @@
+
+
+// Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
+
+  // Session.setDefault('ABC', "nothing yet");
+
+  // Template.messages.helpers = function () {
+  //   return Messages.find({}, { sort: {time: -1} });
+  // };
+
+  // Template.messages.events = function () {
+  //   'ABC': function () {
+  //     return Session.get('ABC');
+  //   }
+  // };
+
 
   Session.setDefault('nprData', "nothing yet");
 
@@ -11,38 +28,36 @@ if (Meteor.isClient) {
 
   Template.retrieve.events({
     'click #fetchButton': function () {
-      Meteor.call('checkNpr', function(err, respJson) {
-        if(err) {
-          window.alert("Error: " + err.reason);
-          console.log("error occured on receiving data on server. ", err);
-        } else {
-          console.log("respJson: ", respJson);
-          Session.set('nprData', respJson);
-        }
+
+      // debugger;
+      // var result = Meteor.call('request');
+
+      Meteor.call('request', function(error, data){
+        Session.set('nprData', data);
+ 
+
       });
-      $('#fetchButton').removeAttr('disabled').val('Fetch');
+
     }
   });
 }
 
 if (Meteor.isServer) {
+
   Meteor.startup(function () {
     // var watson = Meteor.npmRequire('watson-developer-cloud');
     // console.log(watson);
 
+    var url = 'http://api.npr.org/query?date.current&apiKey=MDExMDE5NDY5MDEzNjI4NzgwNTc3MjMxMw001';
+
     Meteor.methods({
-      'checkNpr': function () {
-        var result = Meteor.http.get("http://api.npr.org/query?date.current&apiKey=MDExMDE5NDY5MDEzNjI4NzgwNTc3MjMxMw001&output.JSON", {timeout:30000});
-        if(result.statusCode==200) {
-          var respJson = JSON.parse(result.content);
-          console.log("response received.");
-          return respJson;
-        } else {
-          console.log("Response issue: ", result.statusCode);
-          var errorJson = JSON.parse(result.content);
-          throw new Meteor.Error(result.statusCode, errorJson.error);
-        }
+      'request': function() {
+        return Meteor.http.call('GET', url, {}, function(error, data) {
+          // console.log(error);
+          // console.log(data);
+        });
       }
     });
+  
   });
 }
